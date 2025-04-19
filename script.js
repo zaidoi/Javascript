ea832291f3df1dc0d7ccb9ed3c8838a9a258866b
zaidoi <zaidbadgujar7@gmail.com>
2025-04-19 22:28:35 +0530

    Factory Functions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-let a =10;
+const User = function (name) {
+    this.name = name;
+    this.discordName = "@" + name;
+  }
+  // hey, this is a constructor - 
+  // then this can be refactored into a factory!
+  
+  function createUser (name) {
+    const discordName = "@" + name;
+    return { name, discordName };
+  }
+  // and that's very similar, except since it's just a function,
+  // we don't need a new keyword
+
+  const newObj = createUser("zaid")
+  console.log(newObj);
+  
