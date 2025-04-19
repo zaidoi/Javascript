d6216aea6eff92a3f5e4619781640acecee12f02
zaidoi <zaidbadgujar7@gmail.com>
2025-04-19 22:31:16 +0530

    destructuring

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const User = function (name) {
-    this.name = name;
-    this.discordName = "@" + name;
-  }
-  // hey, this is a constructor - 
-  // then this can be refactored into a factory!
-  
-  function createUser (name) {
-    const discordName = "@" + name;
-    return { name, discordName };
-  }
-  // and that's very similar, except since it's just a function,
-  // we don't need a new keyword
+const obj = { a: 1, b: 2 };
+const { a, b } = obj;
+// This creates two variables, a and b,
+// which are equivalent to
+// const a = obj.a;
+// const b = obj.b;
 
-  const newObj = createUser("zaid")
-  console.log(newObj);
-  
+const array = [1, 2, 3, 4, 5];
+const [ zerothEle, firstEle ] = array;
+// This creates zerothEle and firstEle, both of which point
+// to the elements in the 0th and 1st indices of the array
+console.log(firstEle);
